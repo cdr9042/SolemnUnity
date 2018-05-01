edf86067a630e7f7d8e4c51078c940f7d1dc9797
Unknown <kuroba119@gmail.com>
2018-05-01 22:46:26 +0700

    HealBar-fix

---
+++ Assets/HealBarScriptAni.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealBarScriptAni : MonoBehaviour {
+
+    Image healthBarAni;
+
+
+    float maxHealth = 100f;
+    public static float healthAni;
+
+
+    void Start()
+    {
+        healthBarAni = GetComponent<Image>();
+        healthAni = maxHealth;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        healthBarAni.fillAmount = healthAni / maxHealth;
+        if (healthAni > HealBarScript.health)
+        {
+            healthAni -= 0.1f;
+        }
+        
+        
+	}
+}
